ee723f54e83c5648963fde75fcfe0236d38650fd
Thibaut-Le-Goff <90935476+Thibaut-Le-Goff@users.noreply.github.com>
2022-08-03 16:18:58 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub mod runst {
 	//pub fn weight_init(column: usize) -> &'static usize {
-	pub fn weight_init(column: usize) -> &'static Vec<f64> {
+	pub fn weight_init(column: usize) -> Vec<f64> {
 	//pub fn weight_init(column: usize) {
 		use rand::{thread_rng, Rng};
 				
@@ -15,7 +15,7 @@ pub mod runst {
 		for i in 0..= (column - 1) {		
 			let rand: f64 = rng.gen_range(0.0..=1.0);
 			m[i] = rand;
-			//println!("{:?}", i);
+			//println!("{:?}", m[i]);
 		}
 		
 		m
@@ -23,8 +23,10 @@ pub mod runst {
 }
 
 fn main() {
-	let test: &Vec<f64> = runst::weight_init(5);
+	let test: Vec<f64> = runst::weight_init(5);
+	//let test = runst::weight_init(4);
 	
+    //println!("{:?}", test);
 	for i in test {
 		println!("{:?}", i);
 	}	
