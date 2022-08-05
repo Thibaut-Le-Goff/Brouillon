b4b9d44e6002189e6f47c581f65f6e0afd0d4f44
Thibaut-Le-Goff <90935476+Thibaut-Le-Goff@users.noreply.github.com>
2022-08-05 11:13:47 +0200

    Add files via upload

--- lib-runst/src/main.rs
+++ lib-runst/src/main.rs
@@ -1,23 +1,17 @@
 pub mod runst {
-	pub fn weight_init(row: usize, column: usize) -> Vec<Vec<f64>> {//
-	//pub fn weight_init(column: usize) -> Vec<f64> {
+	pub fn weight_init(row: usize, column: usize) -> Vec<Vec<f64>> {
 		use rand::{thread_rng, Rng};
 				
-		//let mut m: Vec<f64> = vec![0.0; column];
-		let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];//
+		let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];
 		let mut rng = thread_rng();
 
 		for i in 0..= (row - 1) {	
-            		//let add_row: Vec<f64> = vec![0.0; row];//
-            		//matrix[i] = add_row;//
-            		for j in 0..= (column - 1) {//
+            		for j in 0..= (column - 1) {
 				let rand: f64 = rng.gen_range(0.0..=1.0);
-				//m[i] = rand;
-            			matrix[i][j] = rand;//
-            		}//
+            			matrix[i][j] = rand;
+            		}
 		}
-		//m
-		matrix//
+		matrix
 	}
 }
 
@@ -27,36 +21,43 @@ fn main() {
 
 	let test: Vec<Vec<f64>> = runst::weight_init(row, column);
 
-//	for (i, j) in &test {
-//		println!("{:?}", j);
-//	}
+    for i in 0..= (row - 1) {
+        println!("{:?}", test[i]);
+    }
 
 	for i in 0..= (row - 1) {
-		for j in 0..= (column -1)  {
+		for j in 0..= (column - 1)  {
 			println!("{:?}", test[i][j]);
 		}
 	}	
+    
+    println!("test de calcul :");
 
-//	println!("tests de calcule :");
-//	println!(" + 1 :");
-//	for i in &test {
-//		for j in &test {
-//			println!("{:?}", (j + 1.0));
-//		}
-//	}
+    println!(" + 1 :");
+	for i in 0..= (row - 1) {
+		for j in 0..= (column - 1)  {
+			println!("{:?}", (test[i][j] + 1.0));
+		}
+	}	
 
-    	//println!(" - 1 :");
-	//for i in &test {
-	//	println!("{:?}", (i - 1.0));
-//	}	
+    println!(" - 1 :");
+	for i in 0..= (row - 1) {
+		for j in 0..= (column - 1)  {
+			println!("{:?}", (test[i][j] - 1.0));
+		}
+	}
 
-  //  	println!(" * 2 :");
-//	for i in &test {
-//		println!("{:?}", (i * 2.0));
-//	}
+    println!(" * 2 :");
+	for i in 0..= (row - 1) {
+		for j in 0..= (column - 1)  {
+			println!("{:?}", (test[i][j] * 2.0));
+		}
+	}
 
-  //  	println!(" / 2 :");
-//	for i in &test {
-//		println!("{:?}", (i / 2.0));
-//	}	
+    println!(" / 2 :");
+	for i in 0..= (row - 1) {
+		for j in 0..= (column - 1)  {
+			println!("{:?}", (test[i][j] / 2.0));
+		}
+	}	
 }
