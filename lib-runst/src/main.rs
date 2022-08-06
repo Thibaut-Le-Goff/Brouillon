7d600d696dd7a1a61f70b1d0defbbb048f93bf28
Thibaut-Le-Goff <90935476+Thibaut-Le-Goff@users.noreply.github.com>
2022-08-06 16:53:48 +0200

    Add files via upload

--- lib-runst/src/main.rs
+++ lib-runst/src/main.rs
@@ -1,25 +1,47 @@
 pub mod runst {
-	pub fn weight_init(row: usize, column: usize) -> Vec<Vec<f64>> {
-		use rand::{thread_rng, Rng};
+    pub mod weight_init {
+	    pub fn normal_dis(column: usize, row: usize) -> Vec<Vec<f64>> {
+		    use rand::{thread_rng, Rng};
 				
-		let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];
-		let mut rng = thread_rng();
+		    let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];
+		    let mut rng = thread_rng();
+            let a: f64 = 0.0;
+            let b: f64 = 1.0;
 
-		for i in 0..= (row - 1) {	
-            		for j in 0..= (column - 1) {
-				let rand: f64 = rng.gen_range(0.0..=1.0);
-            			matrix[i][j] = rand;
-            		}
-		}
-		matrix
-	}
+	    	for i in 0..= (row - 1) {	
+                for j in 0..= (column - 1) {
+				    let raand: f64 = rng.gen_range(a..=b);
+        	    	matrix[i][j] = rand;
+        		}
+		    }
+		    return matrix;
+        }
+
+	    pub fn uniform_dis(column: usize, row: usize) -> Vec<Vec<f64>> {
+		    use rand::{thread_rng, Rng};
+            //use std::num;
+				
+		    let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];
+		    let mut rng = thread_rng();
+            let mut a: f64 = -1.0 / sqrt(column);
+            let mut b: f64 = 1.0 / sqrt(column);
+
+	    	for i in 0..= (row - 1) {	
+            	for j in 0..= (column - 1) {
+				    let rand: f64 = rng.gen_range(a..=b);
+        	    	matrix[i][j] = rand;
+        		}
+		    }
+		    return matrix;
+        }
+    }
 }
 
 fn main() {
-	let row: usize = 3;
-	let column: usize = 2;
+	let row: usize = 3; // nb neurones layer n
+	let column: usize = 2; // nb neurones layer n-1, fan_in
 
-	let test: Vec<Vec<f64>> = runst::weight_init(row, column);
+	let test: Vec<Vec<f64>> = runst::weight_init::uniform_dis(column, row);
 
     for i in 0..= (row - 1) {
         println!("{:?}", test[i]);
@@ -27,7 +49,7 @@ fn main() {
 
 	for i in 0..= (row - 1) {
 		for j in 0..= (column - 1)  {
-			println!("{:?}", test[i][j]);
+			println!("{}", test[i][j]);
 		}
 	}	
     
@@ -36,28 +58,28 @@ fn main() {
     println!(" + 1 :");
 	for i in 0..= (row - 1) {
 		for j in 0..= (column - 1)  {
-			println!("{:?}", (test[i][j] + 1.0));
+			println!("{}", (test[i][j] + 1.0));
 		}
 	}	
 
     println!(" - 1 :");
 	for i in 0..= (row - 1) {
 		for j in 0..= (column - 1)  {
-			println!("{:?}", (test[i][j] - 1.0));
+			println!("{}", (test[i][j] - 1.0));
 		}
 	}
 
     println!(" * 2 :");
 	for i in 0..= (row - 1) {
 		for j in 0..= (column - 1)  {
-			println!("{:?}", (test[i][j] * 2.0));
+			println!("{}", (test[i][j] * 2.0));
 		}
 	}
 
     println!(" / 2 :");
 	for i in 0..= (row - 1) {
 		for j in 0..= (column - 1)  {
-			println!("{:?}", (test[i][j] / 2.0));
+			println!("{}", (test[i][j] / 2.0));
 		}
 	}	
 }
