1c342e3a7dfabc1969d43af2ab3fa7f16e398d44
Thibaut-Le-Goff <90935476+Thibaut-Le-Goff@users.noreply.github.com>
2022-08-08 19:09:54 +0200

    Add files via upload

--- lib-runst/src/main.rs
+++ lib-runst/src/main.rs
@@ -13,13 +13,11 @@ pub mod runst {
     }
 
 	pub mod weight_init {
-		pub fn normal_dis(column: usize, row: usize) -> Vec<Vec<f64>> {
+        pub fn random(column: usize, row: usize, a: f64, b: f64) -> Vec<Vec<f64>> {
 			use rand::{thread_rng, Rng};
 				
 			let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];
 			let mut rng = thread_rng();
-            let a: f64 = 0.0;
-            let b: f64 = 1.0;
 
 	    	for i in 0..= (row - 1) {	
                 for j in 0..= (column - 1) {
@@ -30,89 +28,52 @@ pub mod runst {
 			return matrix;
         }
 
+		pub fn normal_dis(column: usize, row: usize) -> Vec<Vec<f64>> {
+            let a: f64 = 0.0;
+            let b: f64 = 1.0;
+	        let matrix: Vec<Vec<f64>> = random(column, row, a, b);
+
+			return matrix;
+        }
+
 		pub fn uniform_dis(column: usize, row: usize) -> Vec<Vec<f64>> {
-			use rand::{thread_rng, Rng};
-				
-		    let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];
-		    let mut rng = thread_rng();
             let a: f64 = -1.0 / (column as f64).sqrt();
             let b: f64 = 1.0 / (column as f64).sqrt();
             // ;sqrt() only works with float
-
-	    	for i in 0..= (row - 1) {	
-            	for j in 0..= (column - 1) {
-			    	let rand: f64 = rng.gen_range(a..=b);
-				    matrix[i][j] = rand;
-        		}
-		    }	
-		    return matrix;
+	        let matrix: Vec<Vec<f64>> = random(column, row, a, b);
+		    
+            return matrix;
         }
 
 		pub fn xav_gro_normal_dis(column: usize, row: usize, fan_out: usize) -> Vec<Vec<f64>> {
-			use rand::{thread_rng, Rng};
-				
-		    let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];
-		    let mut rng = thread_rng();
             let a: f64 = 0.0;
             let b: f64 = (2.0 / (column as f64) + (fan_out as f64)).sqrt();
-
-	    	for i in 0..= (row - 1) {	
-            	for j in 0..= (column - 1) {
-			    	let rand: f64 = rng.gen_range(a..=b);
-				    matrix[i][j] = rand;
-        		}
-		    }	
-		    return matrix;
+	        let matrix: Vec<Vec<f64>> = random(column, row, a, b);
+		    
+            return matrix;
         }
 
 		pub fn xav_gro_uniform_dis(column: usize, row: usize, fan_out: usize) -> Vec<Vec<f64>> {
-			use rand::{thread_rng, Rng};
-				
-		    let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];
-		    let mut rng = thread_rng();
             let a: f64 = -(6_f64.sqrt()) / ((column as f64) + (fan_out as f64)).sqrt();
             let b: f64 = 6_f64.sqrt() / ((column as f64) + (fan_out as f64)).sqrt();
-
-	    	for i in 0..= (row - 1) {	
-            	for j in 0..= (column - 1) {
-			    	let rand: f64 = rng.gen_range(a..=b);
-				    matrix[i][j] = rand;
-        		}
-		    }	
-		    return matrix;
+	        let matrix: Vec<Vec<f64>> = random(column, row, a, b);
+		    
+            return matrix;
         }
 
 		pub fn he_normal_dis(column: usize, row: usize) -> Vec<Vec<f64>> {
-			use rand::{thread_rng, Rng};
-				
-		    let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];
-		    let mut rng = thread_rng();
             let a: f64 = 0.0;
             let b: f64 = (2.0 / (column as f64)).sqrt();
-
-	    	for i in 0..= (row - 1) {	
-            	for j in 0..= (column - 1) {
-			    	let rand: f64 = rng.gen_range(a..=b);
-				    matrix[i][j] = rand;
-        		}
-		    }	
-		    return matrix;
+	        let matrix: Vec<Vec<f64>> = random(column, row, a, b);
+		    
+            return matrix;
         }
 
 		pub fn he_uniform_dis(column: usize, row: usize) -> Vec<Vec<f64>> {
-			use rand::{thread_rng, Rng};
-				
-		    let mut matrix: Vec<Vec<f64>> = vec![vec![0.0; column]; row];
-		    let mut rng = thread_rng();
             let a: f64 = -(6.0 / (column as f64)).sqrt();
             let b: f64 = (6.0 / (column as f64)).sqrt();
+	        let matrix: Vec<Vec<f64>> = random(column, row, a, b);
 
-	    	for i in 0..= (row - 1) {	
-            	for j in 0..= (column - 1) {
-			    	let rand: f64 = rng.gen_range(a..=b);
-				    matrix[i][j] = rand;
-        		}
-		    }	
 		    return matrix;
         }
     }
@@ -122,7 +83,7 @@ fn main() {
 	let row: usize = 3; // nb neurones layer n
 	let column: usize = 2; // nb neurones layer n-1, fan_in
 
-	let test: Vec<Vec<f64>> = runst::weight_init::he_uniform_dis(column, row);
+	let test: Vec<Vec<f64>> = runst::weight_init::normal_dis(column, row);
 
 	for i in 0..= (row - 1) {
         	println!("{:?}", test[i]);
